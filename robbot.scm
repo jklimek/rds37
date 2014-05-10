8d343134a77a448300a7b9bc79f72696d842b6a7
drcz <drcz@o2.pl>
2014-05-10 23:21:54 +0200

    timer w osobnem dla wygody

--- robbot.scm
+++ robbot.scm
@@ -704,7 +704,7 @@
 
 (define *state* (restart-world 'cokolwiek))
 
-(add-timer! 80
+(add-timer! (include "timer.scm")
 	    (lambda()
 ;	      (write (if (pair? *general-game-state*) (car *general-game-state*) *general-game-state*)) (newline)
 	      (match *general-game-state*
